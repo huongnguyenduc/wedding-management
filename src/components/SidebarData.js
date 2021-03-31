36786361a2027abfc8805338b6e383646cfe1855
19521592 <38987479+19521592@users.noreply.github.com>
2021-03-31 17:07:46 +0700

    First commit

---
+++ src/components/SidebarData.js
@@ -0,0 +1,50 @@
+import * as GiIcons from "react-icons/gi";
+import * as MdIcons from "react-icons/md";
+import * as BiIcons from "react-icons/bi";
+import * as IoIcons from "react-icons/io5";
+
+export const SidebarData = [
+    {
+        title: 'Bảng điều khiển',
+        path: '/',
+        icon: <MdIcons.MdDashboard />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Sảnh',
+        path: '/lobby',
+        icon: <GiIcons.GiTempleDoor />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Hóa đơn',
+        path: '/',
+        icon: <BiIcons.BiReceipt />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Doanh thu',
+        path: '/',
+        icon: <GiIcons.GiReceiveMoney />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Món ăn',
+        path: '/',
+        icon: <IoIcons.IoFastFoodOutline />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Dịch vụ',
+        path: '/',
+        icon: <BiIcons.BiShapeTriangle />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Quy định',
+        path: '/',
+        icon: <GiIcons.GiFlatHammer />,
+        cName: 'nav-text'
+    },
+
+];
---
+++ src/pages/Lobby.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Lobby() {
+    return (
+        <div className="lobby">
+            <h1>Lobby Page</h1>
+        </div>
+    )
+}
+
+export default Lobby;
